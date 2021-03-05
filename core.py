897ef4676fdc2d4bd432ea9fd0452d11b59a36d9
Liam <william.ghilardi@gmail.com>
2021-03-05 17:38:33 +0100

    refactoring, use of numpy instead of python arrays

---
+++ core.py
@@ -0,0 +1,108 @@
+import random
+import settings
+import numpy as np
+
+class Core():
+
+    def init_game(self,n):
+        #fill the nxn-matrix with 0s
+        matrix = np.zeros((n*n), dtype=int).reshape(n,n)
+        matrix = self.add_two(matrix)
+        matrix = self.add_two(matrix)
+        return matrix
+
+    def add_two(self,matrix):
+        num_rows, num_cols = np.array(matrix).shape
+        while(True):
+            i = random.randint(0, num_rows-1)
+            j = random.randint(0, num_cols-1)
+            if matrix[i][j] == 0:
+                tile_value = 2 if random.random() <= 0.9 else 4
+                matrix[i][j] = tile_value
+                break
+        return matrix
+
+    def game_state(self,matrix):
+        # check for win cell
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 2048:
+                    return 'win'
+        # check for any zero entries
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    return 'not over'
+        # check for same cells that touch each other
+        for i in range(len(matrix)-1):
+            # intentionally reduced to check the row on the right and below
+            # more elegant to use exceptions but most likely this will be their solution
+            for j in range(len(matrix[0])-1):
+                if matrix[i][j] == matrix[i+1][j] or matrix[i][j+1] == matrix[i][j]:
+                    return 'not over'
+        for k in range(len(matrix)-1):  # to check the left/right entries on the last row
+            if matrix[len(matrix)-1][k] == matrix[len(matrix)-1][k+1]:
+                return 'not over'
+        for j in range(len(matrix)-1):  # check up/down entries on last column
+            if matrix[j][len(matrix)-1] == matrix[j+1][len(matrix)-1]:
+                return 'not over'
+        return 'lose'
+
+    def flip_matrix(self,matrix):
+        new = np.flip(matrix,1)
+        return new
+
+    def put_non_zero_to_the_left(self,matrix):
+        col,row = np.array(matrix).shape
+        new = np.zeros((col*row), dtype=int).reshape(col,row)
+        done = False
+        for i in range(settings.GRID_LEN):
+            count = 0
+            for j in range(settings.GRID_LEN):
+                if matrix[i][j] != 0:
+                    new[i][count] = matrix[i][j]
+                    if j != count:
+                        done = True
+                    count += 1
+        return new, done
+
+    def merge_matrix(self,matrix, done):
+        for i in range(settings.GRID_LEN):
+            for j in range(settings.GRID_LEN-1):
+                if matrix[i][j] == matrix[i][j+1] and matrix[i][j] != 0:
+                    matrix[i][j] *= 2
+                    matrix[i][j+1] = 0
+                    done = True
+        return matrix, done
+
+    def shift_left(self,matrix):
+        print("left")
+        matrix, done = self.put_non_zero_to_the_left(matrix)
+        matrix, done = self.merge_matrix(matrix, done)
+        matrix = self.put_non_zero_to_the_left(matrix)[0]
+        matrix= np.array(matrix)
+        if(done):
+            self.add_two(matrix)
+        return matrix, done
+
+    def shift_up(self,matrix):
+        print("up")
+        matrix = matrix.transpose()
+        matrix, done = self.shift_left(matrix)
+        matrix = matrix.transpose()
+        return matrix, done
+
+    def shift_down(self,matrix):
+        print("down")
+        matrix = self.flip_matrix(matrix.transpose())
+        matrix, done = self.shift_left(matrix)
+        matrix = self.flip_matrix(matrix)
+        matrix = matrix.transpose()
+        return matrix, done
+
+    def shift_right(self,matrix):
+        print("right")
+        matrix = self.flip_matrix(matrix)
+        matrix, done = self.shift_left(matrix)
+        matrix = self.flip_matrix(matrix)
+        return matrix, done
--- puzzle.py
+++ puzzle.py
@@ -1,12 +1,13 @@
 from tkinter import Frame, Label, CENTER
 import random
 
-import logic
-import constants as c
+import core 
+import settings
+import numpy as np
 
 
 def gen():
-    return random.randint(0, c.GRID_LEN - 1)
+    return random.randint(0, settings.GRID_LEN - 1)
 
 
 class GameGrid(Frame):
@@ -17,72 +18,73 @@ class GameGrid(Frame):
         self.master.title('2048')
         self.master.bind("<Key>", self.key_down)
 
-        self.commands = {c.KEY_UP: logic.up, c.KEY_DOWN: logic.down,
-                         c.KEY_LEFT: logic.left, c.KEY_RIGHT: logic.right,
-                         c.KEY_UP_ALT: logic.up, c.KEY_DOWN_ALT: logic.down,
-                         c.KEY_LEFT_ALT: logic.left, c.KEY_RIGHT_ALT: logic.right,
-                         c.KEY_H: logic.left, c.KEY_L: logic.right,
-                         c.KEY_K: logic.up, c.KEY_J: logic.down}
+        self.core = core.Core()
+
+        self.commands = {settings.KEY_UP: self.core.shift_up, settings.KEY_DOWN: self.core.shift_down,
+                         settings.KEY_LEFT: self.core.shift_left, settings.KEY_RIGHT: self.core.shift_right,
+                         settings.KEY_UP_ALT: self.core.shift_up, settings.KEY_DOWN_ALT: self.core.shift_down,
+                         settings.KEY_LEFT_ALT: self.core.shift_left, settings.KEY_RIGHT_ALT: self.core.shift_right,
+                         settings.KEY_H: self.core.shift_left, settings.KEY_L: self.core.shift_right,
+                         settings.KEY_K: self.core.shift_up, settings.KEY_J: self.core.shift_down}
         
         self.grid_cells = []
         self.init_grid()
-        self.matrix = logic.new_game(c.GRID_LEN)
+        self.matrix = self.core.init_game(settings.GRID_LEN)
         self.history_matrixs = []
         self.update_grid_cells()
 
         self.mainloop()
 
     def init_grid(self):
-        background = Frame(self, bg=c.BACKGROUND_COLOR_GAME,
-                           width=c.SIZE, height=c.SIZE)
+        background = Frame(self, bg=settings.BACKGROUND_COLOR_GAME,
+                           width=settings.SIZE, height=settings.SIZE)
         background.grid()
 
-        for i in range(c.GRID_LEN):
+        for i in range(settings.GRID_LEN):
             grid_row = []
-            for j in range(c.GRID_LEN):
-                cell = Frame(background, bg=c.BACKGROUND_COLOR_CELL_EMPTY,
-                             width=c.SIZE / c.GRID_LEN,
-                             height=c.SIZE / c.GRID_LEN)
-                cell.grid(row=i, column=j, padx=c.GRID_PADDING,
-                          pady=c.GRID_PADDING)
+            for j in range(settings.GRID_LEN):
+                cell = Frame(background, bg=settings.BACKGROUND_COLOR_CELL_EMPTY,
+                             width=settings.SIZE / settings.GRID_LEN,
+                             height=settings.SIZE / settings.GRID_LEN)
+                cell.grid(row=i, column=j, padx=settings.GRID_PADDING,
+                          pady=settings.GRID_PADDING)
                 t = Label(master=cell, text="",
-                          bg=c.BACKGROUND_COLOR_CELL_EMPTY,
-                          justify=CENTER, font=c.FONT, width=5, height=2)
+                          bg=settings.BACKGROUND_COLOR_CELL_EMPTY,
+                          justify=CENTER, font=settings.FONT, width=5, height=2)
                 t.grid()
                 grid_row.append(t)
 
             self.grid_cells.append(grid_row)
 
     def update_grid_cells(self):
-        for i in range(c.GRID_LEN):
-            for j in range(c.GRID_LEN):
+        for i in range(settings.GRID_LEN):
+            for j in range(settings.GRID_LEN):
                 new_number = self.matrix[i][j]
                 if new_number == 0:
-                    self.grid_cells[i][j].configure(text="", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                    self.grid_cells[i][j].configure(text="", bg=settings.BACKGROUND_COLOR_CELL_EMPTY)
                 else:
-                    self.grid_cells[i][j].configure(text=str(new_number), bg=c.BACKGROUND_COLOR_DICT[new_number],
-                                                    fg=c.CELL_COLOR_DICT[new_number])
+                    self.grid_cells[i][j].configure(text=str(new_number), bg=settings.BACKGROUND_COLOR_DICT[new_number],
+                                                    fg=settings.CELL_COLOR_DICT[new_number])
         self.update_idletasks()
 
     def key_down(self, event):
         key = repr(event.char)
-        if key == c.KEY_BACK and len(self.history_matrixs) > 1:
-            self.matrix = self.history_matrixs.pop()
+        if key == settings.KEY_BACK and len(self.history_matrixs) > 1:
+            self.matrix = np.array(self.history_matrixs.pop())
             self.update_grid_cells()
             print('back on step total step:', len(self.history_matrixs))
-        elif key in self.commands:
-            self.matrix, done = self.commands[repr(event.char)](self.matrix)
+        elif key in self.commands:            
+            self.matrix, done = self.commands[repr(event.char)](self.matrix)            
             if done:
-                self.matrix = logic.add_two(self.matrix)
                 # record last move
                 self.history_matrixs.append(self.matrix)
                 self.update_grid_cells()
-                if logic.game_state(self.matrix) == 'win':
-                    self.grid_cells[1][1].configure(text="You", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                    self.grid_cells[1][2].configure(text="Win!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                if logic.game_state(self.matrix) == 'lose':
-                    self.grid_cells[1][1].configure(text="You", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
-                    self.grid_cells[1][2].configure(text="Lose!", bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                if self.core.game_state(self.matrix) == 'win':
+                    self.grid_cells[1][1].configure(text="You", bg=settings.BACKGROUND_COLOR_CELL_EMPTY)
+                    self.grid_cells[1][2].configure(text="Win!", bg=settings.BACKGROUND_COLOR_CELL_EMPTY)
+                if self.core.game_state(self.matrix) == 'lose':
+                    self.grid_cells[1][1].configure(text="You", bg=settings.BACKGROUND_COLOR_CELL_EMPTY)
+                    self.grid_cells[1][2].configure(text="Lose!", bg=settings.BACKGROUND_COLOR_CELL_EMPTY)
 
     def generate_next(self):
         index = (gen(), gen())
